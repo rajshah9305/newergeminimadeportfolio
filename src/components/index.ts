5b862214d277dd69f29d0d54737f3ced316da41e
Raj Shah <rajshah9305@gmail.com>
2026-04-18 21:38:19 -0600

    Add index.ts to export all components

---
+++ src/components/index.ts
@@ -0,0 +1,10 @@
+export { default as Navigation } from './Navigation';
+export { default as HeroSection } from './HeroSection';
+export { default as SkillsSection } from './SkillsSection';
+export { default as ProjectsSection } from './ProjectsSection';
+export { default as ExperienceSection } from './ExperienceSection';
+export { default as ContactSection } from './ContactSection';
+export { default as Footer } from './Footer';
+export { default as ScrollToTop } from './ScrollToTop';
+export { default as SectionHeader } from './SectionHeader';
+export { default as BrutalistButton } from './BrutalistButton';
